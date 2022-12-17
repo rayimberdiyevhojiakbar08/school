4483a8cacfa358d9e76e6a9eaecd98e938231425
unknown <rayimberdiyevhojiakbar08@gmail.com>
2022-12-17 18:56:37 +0500

    index full

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ import layout from 'express-ejs-layouts';
 const app = exress();
 
 app.set('view engine', 'ejs');
+
+app.use(exress.json());
+app.use(exress.urlencoded({extended: true}));
+app.use(layout);
+
+app.listen(3000, () => console.log('server is runnning Port:3000'));
