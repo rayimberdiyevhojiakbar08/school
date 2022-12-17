d7cf19df77dc2105cdcbb5af6dc9d4d985e4837c
Obuljon <rahimovabul2@gmail.com>
2022-12-17 19:36:57 +0500

    aboutni yozdim

--- routers/home.router.js
+++ routers/home.router.js
@@ -1,7 +1,10 @@
 import {Router} from 'express';
 import MainhomeRouter from '../controllers/home.controller.js';
+import About from "../controllers/about.controller.js";
+
 const router = new Router();
 
 router.get('/', MainhomeRouter.home);
+router.get("/about", About.aboutHome )
 
 export default router
