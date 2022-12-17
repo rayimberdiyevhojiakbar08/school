cf67aa7588f065da3b19ec34a1c676a4f8110a71
Obuljon <rahimovabul2@gmail.com>
2022-12-18 03:14:19 +0500

    htmlni zattim

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("public")); 
+app.use(express.static(`public`)); 
 app.use(express.json());
 app.use(layout);
 app.use('/', HomeRouter);
