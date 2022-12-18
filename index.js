42946e30ba88d3b0dc04b31452ac9e1896a1cfaa
Rozmuhammad2022 <otushbekbaxtiyorov97@gmail.com>
2022-12-18 18:45:20 +0500

    xatolar1

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 import express from 'express';
 import layout from 'express-ejs-layouts';
 import HomeRouter from './routers/home.router.js';
-<<<<<<< HEAD
 import {resolve} from 'path'
-const app = exress();
 const __dirname = resolve();
-app.set('view engine', 'ejs');
 
-app.use(exress.json());
-app.use(exress.urlencoded({extended: true}));
-app.use(exress.static(__dirname + '/public'))
-=======
 const app = express();
-
+app.set('view engine', 'ejs');
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(`public`)); 
 app.use(express.json());
->>>>>>> 8af4386a6326a58273a5132d15ae94571828ad22
+
 app.use(layout);
 app.use('/', HomeRouter);
 
