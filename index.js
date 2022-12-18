dc171581b2dfb6a465608cb8efd14a242b13bb6a
Obuljon <rahimovabul2@gmail.com>
2022-12-18 20:16:48 +0500

    ss

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 import express from 'express';
 import layout from 'express-ejs-layouts';
 import HomeRouter from './routers/home.router.js';
-import {resolve} from 'path'
-const __dirname = resolve();
-
 const app = express();
-app.set('view engine', 'ejs');
+
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(`public`)); 
 app.use(express.json());
-
 app.use(layout);
 app.use('/', HomeRouter);
 
