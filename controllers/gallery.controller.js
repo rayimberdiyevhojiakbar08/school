ddbe7a952d032b155593349cc606869b0e00fd37
Obuljon <rahimovabul2@gmail.com>
2022-12-18 06:07:52 +0500

    galareya va contactni zattim

---
+++ controllers/gallery.controller.js
@@ -0,0 +1,7 @@
+class Gallery{
+    galleryPage(req, res){
+        res.render("main/gallery")
+    }
+}
+
+export default new Gallery();
--- routers/home.router.js
+++ routers/home.router.js
@@ -1,10 +1,13 @@
 import {Router} from 'express';
 import MainhomeRouter from '../controllers/home.controller.js';
 import About from "../controllers/about.controller.js";
-
+import Gallery from "../controllers/gallery.controller.js";
+import Contact from "../controllers/contact.controller.js";
 const router = new Router();
 
 //router.get('/', MainhomeRouter.home);
 router.get("/", About.aboutHome );
+router.get("/gallery", Gallery.galleryPage);
+router.get("/contact", Contact.contactPage);
 
 export default router
