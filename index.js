a4abe8d155821c735dd0e74f77478edf64641f2c
Rozmuhammad2022 <otushbekbaxtiyorov97@gmail.com>
2022-12-18 18:33:10 +0500

    tarjima

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import exress from 'express';
 import layout from 'express-ejs-layouts';
 import HomeRouter from './routers/home.router.js';
+import {resolve} from 'path'
 const app = exress();
-
+const __dirname = resolve();
 app.set('view engine', 'ejs');
 
 app.use(exress.json());
 app.use(exress.urlencoded({extended: true}));
+app.use(exress.static(__dirname + '/public'))
 app.use(layout);
 app.use('/', HomeRouter);
 
