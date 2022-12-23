b1cdd0da78d3fffe3b1b41ee89b5fecf40c00994
Rozmuhammad2022 <otushbekbaxtiyorov97@gmail.com>
2022-12-23 20:41:17 +0500

    galleriya

--- models/gallery.model.js
+++ models/gallery.model.js
@@ -1,29 +1,46 @@
 export default [
     {
-        imgName:"laborotory1.jpg"
+        imgName:"laborotory1.jpg",
+        name: "Laboratory"
     },
     {
-        imgName:"laborotory2.jpg"
+        imgName:"laborotory2.jpg",
+        name: "Laboratory"
+
     },
     {
-        imgName:"laborotory3.jpg"
+        imgName:"laborotory3.jpg",
+        name: "Laboratory"
+
     },
     {
-        imgName:"infarmatika1.jpg"
+        imgName:"infarmatika1.jpg",
+        name: "Infarmation Class"
+
     },
     {
-        imgName:"infarmatika2.jpg"
+        imgName:"infarmatika2.jpg",
+        name: "Infarmation Class"
+
     },
     {
-        imgName:"laborotory3.jpg"
+        imgName:"laborotory3.jpg",
+        name: "Laboratory"
+
     },
     {
-        imgName:"ruslar1.jpg"
+        imgName:"ruslar1.jpg",
+        name: "Class Room"
+
     },
     {
-        imgName:"ruslar2.jpg"
+        imgName:"ruslar2.jpg",
+        name: "Class Room"
+
     },
     {
-        imgName:"football1.jpg"
+        imgName:"football1.jpg",
+        name: "Football"
+
     },
 ]
