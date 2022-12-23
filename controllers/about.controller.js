a68552bb4e968e11d059413e3649c1742ffa417b
Obuljon <rahimovabul2@gmail.com>
2022-12-24 04:21:14 +0500

    tarihi shahslani siqlni zattim

--- controllers/about.controller.js
+++ controllers/about.controller.js
@@ -4,10 +4,12 @@ import data from "../homedatabase/service.js";
 import slider from "../homedatabase/slider.js";
 import teacherComtent from "../homedatabase/teacher.comtent.js";
 import course from "../homedatabase/course.js";
+import counter from "../homedatabase/counter.js";
+import testimonial from "../homedatabase/testimonial.js";
 
 class About{
     aboutHome(req, res){
-        res.render("main/about", { data, slider, fromblog, teacherComtent, course })
+        res.render("main/about", { data, slider, fromblog, teacherComtent, course, counter, testimonial })
     }
 }
 export default new About()
---
+++ homedatabase/testimonial.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        p:"Ilim uchun sariflagan vaqtingizni va pulingizga hech-qachon achinmaysiz.",
+        img:"testimonial-1.png",
+        name: "Benjamin Franklin",
+        kasp: ""
+    },
+    {
+        p:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem rerum soluta aperiam blanditiis obcaecati eveniet aliquam consequatur nobis eaque id.",
+        img:"testimonial-3.png",
+        name:"Rebaca Michel",
+        kasp:""
+    },
+    {
+        p:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem rerum soluta aperiam blanditiis obcaecati eveniet aliquam consequatur nobis eaque id.",
+        img:"testimonial-3.png",
+        name:"Rebaca Michel",
+        kasp:""
+    }
+]
