5db648913ba3b8d23dceef714a522a70c9a907f0
Rozmuhammad2022 <otushbekbaxtiyorov97@gmail.com>
2022-12-23 20:52:26 +0500

    GaLLeriya

--- models/gallery.model.js
+++ models/gallery.model.js
@@ -1,46 +1,61 @@
 export default [
     {
         imgName:"laborotory1.jpg",
-        name: "Laboratory"
+        name: "Laboratory",
+        Webdisagn:"Veb-dizayn"
     },
     {
         imgName:"laborotory2.jpg",
-        name: "Laboratory"
+        name: "Laboratory",
+        Webdisagn:"Veb-dizayn"
 
     },
     {
         imgName:"laborotory3.jpg",
-        name: "Laboratory"
+        name: "Laboratory",
+        Webdisagn:"Veb-dizayn"
 
     },
     {
         imgName:"infarmatika1.jpg",
-        name: "Infarmation Class"
+        name: "Infarmation Class",
+        Webdisagn:"Veb-dizayn",
 
     },
     {
         imgName:"infarmatika2.jpg",
-        name: "Infarmation Class"
+        name: "Infarmation Class",
+        Webdisagn:"Veb-dizayn"
+
 
     },
     {
         imgName:"laborotory3.jpg",
-        name: "Laboratory"
+        name: "Laboratory",
+        Webdisagn:"Veb-dizayn"
+
 
     },
     {
         imgName:"ruslar1.jpg",
-        name: "Class Room"
+        name: "Class Room",
+        Webdisagn:"Veb-dizayn",
+        
+
 
     },
     {
         imgName:"ruslar2.jpg",
-        name: "Class Room"
+        name: "Class Room",
+        Webdisagn:"Veb-dizayn"
+
 
     },
     {
         imgName:"football1.jpg",
-        name: "Football"
+        name: "Football",
+        Webdisagn:"Veb-dizayn"
+
 
     },
 ]
