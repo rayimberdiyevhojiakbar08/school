c8f1054d662ac2b43200b6961098640a878adf42
Obuljon <rahimovabul2@gmail.com>
2022-12-24 00:42:54 +0500

    pulli curslarni siqlini zattim

--- controllers/about.controller.js
+++ controllers/about.controller.js
@@ -3,10 +3,11 @@ import fromblog from "../homedatabase/fromblog.js";
 import data from "../homedatabase/service.js";
 import slider from "../homedatabase/slider.js";
 import teacherComtent from "../homedatabase/teacher.comtent.js";
+import course from "../homedatabase/course.js";
 
 class About{
     aboutHome(req, res){
-        res.render("main/about", { data, slider, fromblog, teacherComtent })
+        res.render("main/about", { data, slider, fromblog, teacherComtent, course })
     }
 }
 export default new About()
---
+++ homedatabase/course.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        img: "1",
+        fan: "Algebra",
+        kun: "210",
+        h4: "",
+        p: "",
+        narxi: "$165.00"
+    },
+    {
+        img: "2",
+        fan: "Fizika",
+        kun: "210",
+        h4: "",
+        p: "",
+        narxi: "$165.00"
+    },
+    {
+        img: "3",
+        fan: "Kimyo",
+        kun: "210",
+        h4: "",
+        p: "",
+        narxi: "$165.00"
+    },
+    {
+        img: "4",
+        fan: "Biologiya",
+        kun: "102",
+        h4: "",
+        p: "",
+        narxi: "$165.00"
+    },
+    {
+        img: "5",
+        fan: "Geografiya",
+        kun: "75",
+        h4: "",
+        p: "",
+        narxi: "$165.00"
+    },
+    {
+        img: "6",
+        fan: "Tarix",
+        kun: "210",
+        h4: "",
+        p: "",
+        narxi: "$165.00"
+    },
+]
