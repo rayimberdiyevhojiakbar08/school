687de16e9dda6aed7d50b14ddc68f782a69e3d23
azizbekoffical <azizbekrozmetov734@gmail.com>
2022-12-27 16:13:33 +0500

    c

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import express from 'express';
+import { connect } from 'mongoose';
+import session from 'express-session';
 import layout from 'express-ejs-layouts';
 import HomeRouter from './routers/home.router.js';
+connect('mongodb://localhost/maktab', {useNewUrlParser: true, useUnifiedTopology: true});
 const app = express();
 
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(`public`)); 
 app.use(express.json());
 app.use(layout);
+app.use(session({
+    secret: "School",
+    cookie: {maxAge: 1000 * 60 * 60 * 24},
+    resave: false,
+    saveUninitialized: false
+}));
+app.use((req, res, next) => {
+    res.locals.user = req.session.user || "";
+    next();
+})
 app.use('/', HomeRouter);
 
-app.listen(3000, () => console.log('server is runnning Port:3000'));
+app.listen(8080, () => console.log('server is runnning Port:8080'));
